73c5b8ae67d71311ec1dd42353627369d01f9ac8
Iain Campbell <iaincampbell11@gmail.com>
2017-09-26 16:13:24 -0300

    Add files via upload

--- Building.java
+++ Building.java
@@ -1,10 +1,10 @@
+//the Interface for buildings
 package civLife;
-import java.util.List; 
 
 public interface Building {
 	
 	boolean purchaseBuilding(int woodResource, int stoneRescource);
 	String getBuildingName();
 	int getBuildingLevel();
-	List<Integer> getBuildingPrice();// this is iffy maybe use arrays? probably over complicating it
+	String getBuildingPrice();
 }
